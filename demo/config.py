0ecf2e9961927cf2bdfed25c2595bc732e90af6a
sean.rivera <sean.au.rivera@gmail.com>
2019-02-24 22:23:02 +0100

    New brach release

--- demo/config.py
+++
@@ -1,8 +0,0 @@
-# config.py
-import os
-
-# Enable Flask's debugging features. Should be False in production
-DEBUG = True
-
-# Allow flask to use session interfaces
-SECRET_KEY = os.urandom(24)
--- demo/run.py
+++
@@ -1,7 +0,0 @@
-# run.py
-
-from demo import demo
-
-if __name__ == '__main__':
-    demo.run(debug=True, use_debugger=False, use_reloader=False)
-    # demo.run()
