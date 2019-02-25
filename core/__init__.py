7c03634eb96b3aa53498d50b00a183c38bb889ce
sean.rivera <sean.au.rivera@gmail.com>
2019-02-25 16:23:30 +0100

    Minor corrections to codebase

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,5 @@
 from .exceptions import StateException
+from .message import Message
 from .node import Node
 from .probe_node import probe_node
 from .topic import Topic
-
-node_scripts = ['kill_node', 'probe_node']
-service_scripts = []
-topic_scripts = []
-node_name = "/rosploit"
-__all__ = node_scripts + service_scripts + topic_scripts
